9b1ef3a9d2d3918dc24e5fe462461349b2f2b74c
vemacs <gentoo@outlook.com>
2015-01-03 14:33:02 -0700

    connect to redis

--- src/main/java/me/vemacs/vperms/vPermsPlugin.java
+++ src/main/java/me/vemacs/vperms/vPermsPlugin.java
@@ -2,14 +2,25 @@ package me.vemacs.vperms;
 
 import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 public class vPermsPlugin extends JavaPlugin {
     @Getter
     private static vPermsPlugin instance;
+    @Getter
+    private static JedisPool jedisPool;
 
     @Override
     public void onEnable() {
         instance = this;
+        String ip = getConfig().getString("ip");
+        int port = getConfig().getInt("port");
+        String password = getConfig().getString("password");
+        if (password == null || password.equals(""))
+            jedisPool = new JedisPool(new JedisPoolConfig(), ip, port, 0);
+        else
+            jedisPool = new JedisPool(new JedisPoolConfig(), ip, port, 0, password);
     }
 
     @Override
